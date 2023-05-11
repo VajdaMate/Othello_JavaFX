b0532417c3043ccee40e81ebeafe387250d85fd9
Vajda Máté Pál <vajdamate0204@gmail.com>
2023-05-11 15:14:29 +0200

    Refactored GameModel

--- src/main/java/model/GameModel.java
+++ src/main/java/model/GameModel.java
@@ -3,9 +3,7 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class GameModel {
-
     public static int boardSize=8;
     private final Disk[][] gameBoard =new Disk[boardSize][boardSize];
     private boolean currentPlayer;
@@ -67,66 +65,14 @@ public class GameModel {
         return Over;
     }
 
-    private void nextPlayer(){
-        calculateNumbers();
-        if (isGameOver())
-            Over=true;
-        else {
-            currentPlayer = !currentPlayer;
-            validSteps();
-            if (validNumber == 0) {
-                currentPlayer = !currentPlayer;
-                validSteps();
+    public void undo(){
+        if (previousFlips.size()>=1) {
+            ResultOfFlipping prevFlip = previousFlips.remove(previousFlips.size() - 1);
+            gameBoard[prevFlip.getTriggerPosition().getRow()][prevFlip.getTriggerPosition().getColumn()].setColor(Colors.NONE);
+            for (var flip : prevFlip.getFlippedPositions()) {
+                gameBoard[flip.getRow()][flip.getColumn()].setColor(prevFlip.getColor());
             }
-        }
-    }
-
-    public void calculateNumbers(){
-        this.whiteNumber=0;
-        this.blackNumber=0;
-        for (var i = 0; i < boardSize; i++) {
-            for (var j = 0; j < boardSize; j++) {
-                if (gameBoard[i][j].getColor()==Colors.WHITE)
-                    this.whiteNumber++;
-                else if (gameBoard[i][j].getColor()==Colors.BLACK)
-                    this.blackNumber++;
-            }
-        }
-    }
-
-
-    private void resetValid(){
-        for (var row = 0; row < boardSize; row++) {
-            for (var col = 0; col < boardSize; col++) {
-                if (gameBoard[row][col].getColor()==Colors.VALID)
-                    gameBoard[row][col].setColor(Colors.NONE);
-
-            }
-        }
-        validNumber=0;
-    }
-    private void validSteps() {
-        resetValid();
-        ArrayList<Position> possibleSteps = possibleSteps();
-        for (var step : possibleSteps) {
-            var neighbourList=enemyNeighbours(step.getRow(), step.getColumn());
-            for (var vector:neighbourList){
-                    int xAxisDirection = vector.xAxisDirection();
-                    int yAxisDirection = vector.yAxisDirection();
-                    int currentXPosition=step.getRow()+xAxisDirection;
-                    int currentYPosition=step.getColumn()+yAxisDirection;
-                    while (((currentXPosition >= 0) && (currentXPosition <= 7)) &&
-                           ((currentYPosition >= 0) && (currentYPosition <= 7))){
-                        if (gameBoard[currentXPosition][currentYPosition].getColor()==currentColor()){
-                            gameBoard[step.getRow()][step.getColumn()].setColor(Colors.VALID);
-                            validNumber++;
-                        }
-                        else if(gameBoard[currentXPosition][currentYPosition].getColor()==Colors.NONE)
-                            break;
-                        currentXPosition+=xAxisDirection;
-                        currentYPosition+=yAxisDirection;
-                    }
-                }
+            nextPlayer();
         }
     }
 
@@ -142,7 +88,7 @@ public class GameModel {
             int currentXPosition = row + xAxisDirection;
             int currentYPosition = col + yAxisDirection;
             while ((currentXPosition >=0 && currentXPosition <= boardSize-1) &&
-                   (currentYPosition >= 0 && currentYPosition <=boardSize-1)) {
+                    (currentYPosition >= 0 && currentYPosition <=boardSize-1)) {
                 if (gameBoard[currentXPosition][currentYPosition].getColor() == opponentColor()) {
                     needTurning.add(gameBoard[currentXPosition][currentYPosition]);
                     turnedPositions.add(new Position(currentXPosition,currentYPosition));
@@ -164,6 +110,67 @@ public class GameModel {
         nextPlayer();
     }
 
+    private void nextPlayer(){
+        calculateNumbers();
+        if (isGameOver())
+            Over=true;
+        else {
+            currentPlayer = !currentPlayer;
+            validSteps();
+            if (validNumber == 0) {
+                currentPlayer = !currentPlayer;
+                validSteps();
+            }
+        }
+    }
+
+    private void calculateNumbers(){
+        this.whiteNumber=0;
+        this.blackNumber=0;
+        for (var i = 0; i < boardSize; i++) {
+            for (var j = 0; j < boardSize; j++) {
+                if (gameBoard[i][j].getColor()==Colors.WHITE)
+                    this.whiteNumber++;
+                else if (gameBoard[i][j].getColor()==Colors.BLACK)
+                    this.blackNumber++;
+            }
+        }
+    }
+
+    private void validSteps() {
+        resetValid();
+        ArrayList<Position> possibleSteps = possibleSteps();
+        for (var step : possibleSteps) {
+            var neighbourList=enemyNeighbours(step.getRow(), step.getColumn());
+            for (var vector:neighbourList){
+                int xAxisDirection = vector.xAxisDirection();
+                int yAxisDirection = vector.yAxisDirection();
+                int currentXPosition=step.getRow()+xAxisDirection;
+                int currentYPosition=step.getColumn()+yAxisDirection;
+                while (((currentXPosition >= 0) && (currentXPosition <= 7)) &&
+                        ((currentYPosition >= 0) && (currentYPosition <= 7))){
+                    if (gameBoard[currentXPosition][currentYPosition].getColor()==currentColor()){
+                        gameBoard[step.getRow()][step.getColumn()].setColor(Colors.VALID);
+                        validNumber++;
+                    }
+                    else if(gameBoard[currentXPosition][currentYPosition].getColor()==Colors.NONE)
+                        break;
+                    currentXPosition+=xAxisDirection;
+                    currentYPosition+=yAxisDirection;
+                }
+            }
+        }
+    }
+    private void resetValid(){
+        for (var row = 0; row < boardSize; row++) {
+            for (var col = 0; col < boardSize; col++) {
+                if (gameBoard[row][col].getColor()==Colors.VALID)
+                    gameBoard[row][col].setColor(Colors.NONE);
+
+            }
+        }
+        validNumber=0;
+    }
     private ArrayList<Position> possibleSteps(){
         ArrayList<Position> stepsList =new ArrayList<>();
         for (var row = 0; row < boardSize; row++) {
@@ -187,14 +194,6 @@ public class GameModel {
         }
         return directionVectors;
     }
-    public void undo(){
-        if (previousFlips.size()>=1) {
-            ResultOfFlipping prevFlip = previousFlips.remove(previousFlips.size() - 1);
-            gameBoard[prevFlip.getTriggerPosition().getRow()][prevFlip.getTriggerPosition().getColumn()].setColor(Colors.NONE);
-            for (var flip : prevFlip.getFlippedPositions()) {
-                gameBoard[flip.getRow()][flip.getColumn()].setColor(prevFlip.getColor());
-            }
-            nextPlayer();
-        }
-    }
+
+
 }
